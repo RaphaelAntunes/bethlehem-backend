b91faf840d0d80f4305dc223617d5909669f23f5
Raphael Antunes <faelpinheiroantunes@gmail.com>
2023-12-15 21:14:36 -0300

    Melhorias

---
+++ resources/views/pages/modal-ver-lista.blade.php
@@ -0,0 +1,25 @@
+<div class="modal fade" id="exampleModalCenter3" tabindex="-1" role="dialog"
+        aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header d-flex align-items-center">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Suas Listas</h5>
+                    <div class="d-flex justify-content-around aling-items-center">
+                        <button class="btn bnt-success" onclick="modalcriarlista()">Criar Lista</button>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                </div>
+                <div id="grouplistaitens">
+
+                </div>
+
+            </div>
+
+            <div class="modal-footer d-flex justify-content-between">
+
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
